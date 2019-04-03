8ca48a1e16ac5cf9c63c441dc1a84834c7caf8bd
Will Crichton <wcrichto@cs.stanford.edu>
2019-04-03 01:01:20 +0000

    Use tag for version no.

--- rekallpy/setup.py
+++ rekallpy/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 if __name__ == "__main__":
     setup(
         name='rekallpy',
-        version='0.1.0',
+        version_format='{tag}',
         description='Spatiotemporal query language',
         url='http://github.com/scanner-research/rekall',
         author='Dan Fu',
@@ -11,6 +11,6 @@ if __name__ == "__main__":
         license='Apache 2.0',
         packages=['rekall'],
         install_requires=['python-constraint', 'tqdm', 'cloudpickle'],
-        setup_requires=['pytest-runner'],
+        setup_requires=['setuptools-git-version', 'pytest-runner'],
         tests_require=['pytest'],
         zip_safe=False)
