1bde21b7483afb23925165ec8df6444047f34f3b
Will Crichton <wcrichto@cs.stanford.edu>
2019-04-04 14:54:17 -0700

    Add vgrid_utils to setup.py, bump to v0.1.3

--- rekallpy/setup.py
+++ rekallpy/setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 if __name__ == "__main__":
     setup(
         name='rekallpy',
-        version='0.1.2',
+        version='0.1.3',
         description='Spatiotemporal query language',
         url='http://github.com/scanner-research/rekall',
         author='Dan Fu',
         author_email='danfu@stanford.edu',
         license='Apache 2.0',
-        packages=['rekall'],
+        packages=['rekall', 'rekall.vgrid_utils'],
         install_requires=['python-constraint', 'tqdm', 'cloudpickle'],
         setup_requires=['pytest-runner'],
         tests_require=['pytest'],
