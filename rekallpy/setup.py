1ec6189b04c33300cbc00452df4f2528c1f82a2a
Will Crichton <wcrichto@cs.stanford.edu>
2019-04-04 14:25:03 -0700

    Bump to 0.1.2

--- rekallpy/setup.py
+++ rekallpy/setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 if __name__ == "__main__":
     setup(
         name='rekallpy',
-        version='0.1.1',
+        version='0.1.2',
         description='Spatiotemporal query language',
         url='http://github.com/scanner-research/rekall',
         author='Dan Fu',
