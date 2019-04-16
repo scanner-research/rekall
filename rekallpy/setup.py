c4f714bbe413d31b024dbb48833a831ccea42fc4
Dan Fu <danfu09@gmail.com>
2019-04-16 14:31:15 +0000

    Update a few paths and strings

--- rekallpy/setup.py
+++ rekallpy/setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 if __name__ == "__main__":
     setup(
         name='rekallpy',
-        version='0.1.4',
+        version='0.2.0',
         description='Spatiotemporal query language',
         url='http://github.com/scanner-research/rekall',
         author='Dan Fu',
-        author_email='danfu@stanford.edu',
+        author_email='danfu@cs.stanford.edu',
         license='Apache 2.0',
-        packages=['rekall', 'rekall.vgrid'],
+        packages=['rekall'],
         install_requires=['python-constraint', 'tqdm', 'cloudpickle'],
         setup_requires=['pytest-runner'],
         tests_require=['pytest'],
