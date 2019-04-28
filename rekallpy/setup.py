07654e4a24cff213487e02d1dceecd8a5baba543
Will Crichton <wcrichto@cs.stanford.edu>
2019-04-28 15:27:45 -0700

    Bump to 0.2.5

--- rekallpy/setup.py
+++ rekallpy/setup.py
@@ -1,16 +1,15 @@
 from setuptools import setup
 
 if __name__ == "__main__":
-    setup(
-        name='rekallpy',
-        version='0.2.4',
-        description='Spatiotemporal query language',
-        url='http://github.com/scanner-research/rekall',
-        author='Dan Fu',
-        author_email='danfu@cs.stanford.edu',
-        license='Apache 2.0',
-        packages=['rekall', 'rekall.bounds', 'rekall.stdlib'],
-        install_requires=['python-constraint', 'tqdm', 'cloudpickle'],
-        setup_requires=['pytest-runner'],
-        tests_require=['pytest'],
-        zip_safe=False)
+    setup(name='rekallpy',
+          version='0.2.5',
+          description='Spatiotemporal query language',
+          url='http://github.com/scanner-research/rekall',
+          author='Dan Fu',
+          author_email='danfu@cs.stanford.edu',
+          license='Apache 2.0',
+          packages=['rekall', 'rekall.bounds', 'rekall.stdlib'],
+          install_requires=['python-constraint', 'tqdm', 'cloudpickle'],
+          setup_requires=['pytest-runner'],
+          tests_require=['pytest'],
+          zip_safe=False)
